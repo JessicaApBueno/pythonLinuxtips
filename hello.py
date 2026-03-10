31291dba6bf67100e9707463dbb4797883561a7a
Jessica Bueno <buenojessicaaparecida@gmail.com>
2026-03-09 22:12:48 -0300

    Meu script hello world multi-language

--- hello.py
+++ hello.py
@@ -1,6 +1,37 @@
 #!/usr/bin/env python3
+"""Hello World Multi-language
 
-# Este programa imprime  "JESSICA" em letras maiúsculas
+Depending on the language configured in the environment, the program displays 
+the corresponding message.
+
+Usage:
+
+Have the LANG variable properly configured, e.g.:
+
+    export LANG=en_US
+
+Execution:
+
+    python hello.py
+    or
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Jessica Bueno"
+__license__ = "Unlicense"
+
+import os
 
 
-print ('jessica'.upper())
+current_language = os.getenv("LANG", "en_US")[:5]
+msg ="Hello, World"
+
+if current_language == "pt_BR":
+	msg = "Olá, Mundo"
+elif current_language == "it_IT":
+	msg = "Ciao, Mondo!"
+elif current_language == "fr_FR":
+	msg = "Bonjour, Monde!"
+
+print (msg)
+
