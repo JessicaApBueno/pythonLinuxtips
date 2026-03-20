9555562edddd7cf41269eb35c36e080748725b16
Jessica Bueno <buenojessicaaparecida@gmail.com>
2026-03-19 23:30:40 -0300

    refactor hello.py

--- hello.py
+++ hello.py
@@ -1,36 +1,43 @@
 #!/usr/bin/env python3
-"""Hello World Multi-language
+import os
+import sys
 
-Depending on the language configured in the environment, the program displays 
-the corresponding message.
-
-Usage:
-
-Have the LANG variable properly configured, e.g.:
-
-    export LANG=en_US
-
-Execution:
-
-    python hello.py
-    or
-    ./hello.py
-"""
-__version__ = "0.1.1"
+__version__ = "0.1.3"
 __author__ = "Jessica Bueno"
 __license__ = "Unlicense"
 
-import os
-
-
-current_language = os.getenv("LANG", "en_US")[:5]
-
-msg = {
-	"pt_BR": "Olá, Mundo!",
-	"it_IT": "Ciao, Mondo!",
-	"fr_FR": "Bonjour, Monde!",
-	"en_US": "Hello, World!",
+arguments = {
+    "lang": None,
+    "count": None
 }
 
-print (msg[current_language])
+for arg in sys.argv[1:]:
+    try:
+        key, value = arg.split("=")
+        key = key.lstrip("-").strip() # Limpa traços extras como --lang
+        value = value.strip()
+        
+        if key not in arguments:
+            print(f"Invalid Option: {key}")
+            sys.exit(1) # Sai com erro se a opção for inválida
+            
+        arguments[key] = value
+    except ValueError:
+        print("Erro: Use o formato chave=valor (ex: lang=pt_BR)")
+        sys.exit(1)
 
+# Lógica para definir o idioma
+current_language = arguments["lang"]
+if current_language is None:
+    # Pega os 5 primeiros caracteres (ex: pt_BR)
+    current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = {
+    "pt_BR": "Olá, Mundo!",
+    "it_IT": "Ciao, Mondo!",
+    "fr_FR": "Bonjour, Monde!",
+    "en_US": "Hello, World!",
+}
+
+# Uso do .get() para evitar que o programa quebre caso o idioma não exista no dicionário
+print(msg.get(current_language, msg["en_US"]))
